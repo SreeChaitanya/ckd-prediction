53bf16ae6adee48eab1982834b8bb8735cab498b
Swaroop-P <soupspring47@gmail.com>
2017-05-06 20:02:47 +0530

    app listening on port 8080. using IOLoop.httpserver

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ class HomeHandler(tornado.web.RequestHandler):
         data = json.loads(self.request.body)
         print data
         self.write(data)
-
     def get(self):
         print "Homehandler get request"
         self.write("Home")
@@ -29,16 +28,11 @@ def make_app():
         (r"/home", HomeHandler)
     ])
     return application
-    # http_server = tornado.httpserver.HTTPServer(application)
-    # port = int(os.environ.get("PORT", 5000))
-    # http_server.listen(port)
-    # tornado.ioloop.IOLoop.instance().start()
  
 if __name__ == "__main__":
     app = make_app()
     http_server = tornado.httpserver.HTTPServer(make_app())
     port = int(os.environ.get("PORT", 8080))
     http_server.listen(port)
-    #app.listen(5000)
-    print"Listening on port 5000"
+    print"Listening on port : "+(str(port))
     tornado.ioloop.IOLoop.current().start()
