4e2a67555508431b949e421e62fa4a62fc8168ff
Swaroop-P <soupspring47@gmail.com>
2017-05-06 15:29:57 +0530

    Post method added to homehandler

--- main.py
+++ main.py
@@ -2,19 +2,42 @@ import os
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+from tornado import autoreload
+import json
+
+autoreload.start() 
+print 'App Auto Reload...'
  
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello world")
+        print "MainHandler get request"
+
+class HomeHandler(tornado.web.RequestHandler):
+    def post(self):
+        data = json.loads(self.request.body)
+        print data
+        self.write(data)
+
+    def get(self):
+        print "Homehandler get request"
+        self.write("Home")
  
 def main():
     application = tornado.web.Application([
         (r"/", MainHandler),
+        (r"/home", HomeHandler)
     ])
+    return  application
     http_server = tornado.httpserver.HTTPServer(application)
+
     port = int(os.environ.get("PORT", 5000))
+
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
  
 if __name__ == "__main__":
-    main()
+    app = main()
+    app.listen(5000)
+    print"Listening on port 5000"
+    tornado.ioloop.IOLoop.current().start()
