86417ed155bfc9bcd93bf4fb0aa91d365e3b6d0e
CORP\SreeChaitanya.Y <sreechaitanya.y@gmail.com>
2017-05-08 21:14:56 +0530

    Added random forest prediction model and code to read the model and predict

--- bo/Prediction.py
+++ bo/Prediction.py
@@ -1,9 +1,11 @@
 # --- API Controllers ---
 from bo.Base import BO_Base
-
+import scipy
 # --- Logger ---
 from logger import logger_error, logger_info, logger_warning
-
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import pickle
 
 class BO_Prediction(BO_Base):
 
@@ -13,6 +15,10 @@ class BO_Prediction(BO_Base):
     def disease_prediction(self, prediction_params):
 
         # ----> Machine Learning Code goes here
+        dataset = pd.Series(prediction_params)
         logger_info("Code block at Disease Prediction fn.")
         logger_info("Parameters received : " + str(prediction_params))
-        return True
+        rf = pickle.load(open('ckd_prediction_model.sav', 'rb'))
+        Y_pred = rf.predict(dataset)
+
+        return Y_pred[0]
